3d27ac46122c19b1aaf8aa7603eb796ac2bd71a9
renjika19 <f.opesanmi@gmail.com>
2019-03-20 08:38:40 -0400

    Last push before project

--- script/GameOfTheYear.js
+++ script/GameOfTheYear.js
@@ -1,8 +1,18 @@
 var count = 0
-
+var winnerCount = {x:0, o:0}
 
 function gamePlay() {
   document.playersTurn = "X";
+}
+
+function playerWinCount(playerThatWon) {
+  if(playerThatWon == "X") {
+    winnerCount.x++;
+    document.getElementById("xWin").innerHTML = playerThatWon +":"+ winnerCount.x 
+  }else{
+    winnerCount.o++;
+    document.getElementById("oWin").innerHTML = playerThatWon +":"+ winnerCount.o
+  }
 }
 
 
@@ -10,7 +20,7 @@ function playerMessage() {
   document.getElementById("message").innerHTML = document.playersTurn + "'s Turn";
 }
 
-function userInput(sections) {
+function nextMove(sections) {
   if(sections.innerHTML == "") {
     sections.innerHTML = document.playersTurn;
     let winner = getWinner(document.playersTurn); 
@@ -18,18 +28,19 @@ function userInput(sections) {
     changePlayer();
     playerMessage();
     count++
-      if(count === 9) {
-        document.getElementById("message").innerHTML = "DRAW";
-      }
-    } else if(sections.innerText === "X" || sections.innerText === "O"){
-      playerMessage()
-      document.getElementById("message").innerHTML = "Choose Another Square";
+    if(count === 9) {
+      document.getElementById("message").innerHTML = ("DRAW")
+    }
+  } else if(sections.innerText === "X" || sections.innerText === "O"){
+    playerMessage()
+    document.getElementById("message").innerHTML = "Choose Another Square";
   } 
 }
 
 function alertPlayer(winner) {
   if (winner) {
     document.getElementById("message").innerHTML = ("Winner Winner Chicken Dinner");
+    playerWinCount(document.playersTurn)
   }
 }
 
@@ -37,22 +48,12 @@ function restart() {
   var td = document.getElementsByTagName("td");
   for(let i = 0; i < td.length; i++) {
     td[i].innerHTML = "";
+    document.getElementById("message").innerHTML = document.playersTurn + "'s To Start Game";
+    count = 0;
+    gamePlay();
+    
   }
-  //document.getElementsByTagName('td').forEach(this.innerHTML = '');
-  //document.getElementsByTagName('td').fill('');
 }
-
-// function drawCount() {
-//   for(count= 0; count < 9; count++){
-//     if(count == 8 || getWinner()){
-//        break;
-//     } else if(count == 9) {
-//       playerMessage()
-//       document.getElementById("message").innerHTML = ("STALEMATE");
-//     }
-//   }
-// return false; 
-// }
 
 function changePlayer() {
   document.playersTurn = (document.playersTurn=="X")?"O":"X";
@@ -65,12 +66,9 @@ function getWinner(playersTurn) {
   else if(document.getElementById("A2").innerText === playersTurn && document.getElementById("B2").innerText === playersTurn && document.getElementById("C2").innerText === playersTurn)return true;
   else if(document.getElementById("A3").innerText === playersTurn && document.getElementById("B3").innerText === playersTurn && document.getElementById("C3").innerText === playersTurn)return true;
   else if(document.getElementById("B1").innerText === playersTurn && document.getElementById("B2").innerText === playersTurn && document.getElementById("B3").innerText === playersTurn)return true;
-  else if(document.getElementById("A2").innerText === playersTurn && document.getElementById("B2").innerText === playersTurn && document.getElementById("C1").innerText === playersTurn)return true;
+  else if(document.getElementById("A2").innerText === playersTurn && document.getElementById("B2").innerText === playersTurn && document.getElementById("C2").innerText === playersTurn)return true;
   else if(document.getElementById("C1").innerText === playersTurn && document.getElementById("C2").innerText === playersTurn && document.getElementById("C3").innerText === playersTurn)return true;
   return false;
 
 }
 
-function myButton() {
-  location.reload();
-}
